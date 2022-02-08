a77387e23646532e05034572cbf384d5a94cccf6
Ahmed Labeeb <labeeb.edu123@gmail.com>
2022-02-08 18:14:28 +0600

    modal in progreass

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import { Routes, Route, Link } from "react-router-dom";
-import '../src/App.css'
+import LoginModal from "./Components/Common/LoginModal";
 
-import NavBar from './Components/Common/NavBar';
+import "../src/App.css";
+
+import NavBar from "./Components/Common/NavBar";
 
 function App() {
   return (
     <div className="App">
-       <NavBar/>
+      <NavBar />
+      <LoginModal />
     </div>
   );
 }
---
+++ src/Components/Common/LoginModal.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const LoginModal = () => {
+  return (
+    <div className="modal-backdrop">
+      <div className="modal">
+       <h2>10% Off Coupon Code!!</h2>
+       <p>10% Off Coupon Code!!</p>
+      </div>
+    </div>
+  );
+};
+
+export default LoginModal;
