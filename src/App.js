6a6c8e33af19da8c4065ccd6ebde4f1e51d6acb3
Ahmed Labeeb <labeeb.edu123@gmail.com>
2022-02-09 14:06:50 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ import NavBar from "./Components/Common/NavBar";
 function App() {
   return (
     <div className="App">
-      <NavBar />
       <LoginModal />
+      <NavBar />
     </div>
   );
 }
