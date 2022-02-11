decc995f96bf3fef6feb065f396b65d5773e888e
Ahmed Labeeb <labeeb.edu123@gmail.com>
2022-02-11 18:04:23 +0600

    third section one

---
+++ src/FirebaseAuth/Firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC4sWEffrvsfGxQgtLD3zp0_Zj0DQyl0Jk",
+  authDomain: "course-cloud-react.firebaseapp.com",
+  projectId: "course-cloud-react",
+  storageBucket: "course-cloud-react.appspot.com",
+  messagingSenderId: "588688052049",
+  appId: "1:588688052049:web:21c91247f5112ed8210d70",
+};
+
+initializeApp(firebaseConfig);
+
+const useFireBase = () => {};
+
+export default useFireBase;
