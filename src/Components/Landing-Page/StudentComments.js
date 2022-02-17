1e76c646a1369616e1c7fdd0611b05425247579e
Ahmed Labeeb <labeeb.edu123@gmail.com>
2022-02-18 02:42:12 +0600

    first commit

--- src/Components/Landing-Page/StudentComments.js
+++ src/Components/Landing-Page/StudentComments.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import StudentComment from "../../Assets/StudentComment.png"
+
 
 const StudentComments = () => {
   return (
     <div className="comments-box">
     <Container>
-       <img src={StudentComment} alt="StudentComments" className="w-75 ms-5"/>
+
     </Container>
     </div>
   )
