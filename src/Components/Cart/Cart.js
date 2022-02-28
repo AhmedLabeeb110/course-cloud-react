f4b5613d47d6ed194b794fb924f773f9d472fce8
Ahmed Labeeb <labeeb.edu123@gmail.com>
2022-02-28 13:08:34 +0600

    do

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -3,9 +3,14 @@ import { Container, Row, Col } from "react-bootstrap";
 
 const Cart = () => {
   return (
-    <div className="cart-main p-5 w-100">
+    <div>
+      <div className="cart-main p-5 w-100">
         <p>Home / My Cart</p>
         <h3>My Cart</h3>
+      </div>
+      <div className="card-products p-5 w-100">
+        <h3>Lorem</h3>
+      </div>
     </div>
   );
 };
