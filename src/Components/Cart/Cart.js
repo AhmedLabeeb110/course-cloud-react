428b82822d16146e19fc932830dd7d16cc8e964e
Ahmed Labeeb <labeeb.edu123@gmail.com>
2022-03-10 11:55:19 +0600

    cart progress

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -2,10 +2,8 @@ import React, { useEffect, useState } from "react";
 import { Container } from "react-bootstrap";
 
 import { connect } from "react-redux";
-import { removeFromCart, adjustItemQty } from "../../redux/Shopping/shopping-actions";
 
-const Cart = ({cart, removeFromCart}) => {
-  console.log(cart)
+const Cart = ({cart}) => {
   // const [courses, setCourses] = useState([]);
   // useEffect(() => {
   //   fetch("db.json")
@@ -22,7 +20,7 @@ const Cart = ({cart, removeFromCart}) => {
     let items = 0;
     let price = 0;
 
-    cart?.forEach((item) => {
+    cart.forEach((item) => {
       items += item.qty;
       price += item.qty * item.price;
     });
@@ -34,33 +32,30 @@ const Cart = ({cart, removeFromCart}) => {
   return (
   <div className="cart-main">
       <Container className="p-5">
-      <p>Home / MyCart</p>
-      <h3>Cart</h3>
-      {cart?.map((item) => (
-      <div className="cart-itens" key={item.id}>
-        <div className="added-items border border-1 w-50">
-          <img src="" alt="" />
-          <p>{item.title}</p>
-          <input value={item.qty} />
-          <p onClick={() => removeFromCart(item.id)}>remove</p>
-        </div>
-      </div>))}
+        <p>Home / MyCart</p>
+        <h3>Cart</h3>
+        {cart.map((item) => (
+        <div className="cart-itens" key={item.id}>
+          <div className="added-items border border-1 w-50">
+            <img src="" alt="" />
+            <p>{item.title}</p>
+            <input value={item.qty} />
+          </div>
+        </div>))}
 
-      <div className="sub-total">
-        <h6>Total: ({totalItems})</h6>
-        <h6>£: ({totalPrice})</h6>
-      </div>
+        <div className="sub-total">
+          <h6>Total: ({totalItems})</h6>
+          <h6>Total: ({totalPrice})</h6>
+        </div>
       </Container>
     </div>
   );
 };
 
-const mapStateToProps = (state, dispatch) => {
+const mapStateToProps = (state) => {
   return {
-    cart: state?.shop?.cart,
-    adjustQty: (id, value) => dispatch(adjustItemQty(id, value)),
-    removeFromCart: (id) => dispatch(removeFromCart(id)),
+    cart: state.shop.cart,
   };
 };
 
-export default connect(null, mapStateToProps)(Cart);
+export default connect(mapStateToProps)(Cart);
